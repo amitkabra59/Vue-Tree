645bf8dae50e2f3f6a6c577599a10d28ee152018
Amit Kabra <amitkabra59@gmail.com>
2021-01-04 15:35:09 +0530

    Vue tree

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-col class="d-flex justify-space-around">
+        <v-toolbar-title class="white--text">VUE TREE</v-toolbar-title>
+      </v-col>
+    </v-app-bar>
+
+    <v-main>
+      <h1>{{ this.content }}</h1>
+      <Tree :node="root" @onClick="nodeWasClicked"></Tree
+    ></v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Tree from "./components/Tree";
+import root from "./data.json";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+
+  components: { Tree },
+  methods: {
+    nodeWasClicked(node) {
+      this.content = node.name;
+    },
+  },
+
+  data: () => ({
+    content: "",
+    root,
+  }),
+};
 </script>
 
+
 <style>
+body {
+  background-color: #333;
+  color: white;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
